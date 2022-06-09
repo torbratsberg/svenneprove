20155011af7d95d4a364ab5ae3d62b5ea4623315
Tor Bratsberg <tor@bratsberg.net>
2022-06-09 20:02:33 +0200

    Refactor(CenteredText): Redid animations

--- app/blocks/CenteredText.js
+++ app/blocks/CenteredText.js
@@ -5,10 +5,10 @@ function CenteredText({ block }) {
     return (
         <div className="CenteredText">
             <div className="row">
-                <div className="columns small-12 large-4 large-offset-4 om fade-up">
-                    {block.title && <h2>{block.title}</h2>}
-                    {block.text && <p>{block.text}</p>}
-                        {block.link && <Link className="large" href={block.link.url}><a className="btn large">{block.link.label}</a></Link>}
+                <div className="columns small-12 large-4 large-offset-4">
+                    {block.title && <h2 className="om dash">{block.title}</h2>}
+                    {block.text && <p className="om fade-up">{block.text}</p>}
+                    {block.link && <Link className="large" href={block.link.url}><a className="btn large om fade-up">{block.link.label}</a></Link>}
                 </div>
             </div>
         </div>
