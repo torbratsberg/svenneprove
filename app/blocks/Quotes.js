d2def12abc62becf77ff6511e8a0410e43ba8dbe
Tor Bratsberg <tor@bratsberg.net>
2022-06-10 09:25:46 +0200

    Feat(quotes): Add actual quotes to the quotes block

--- app/blocks/Quotes.js
+++ app/blocks/Quotes.js
@@ -5,7 +5,7 @@ function Quotes({ block }) {
             <div className="row">
                 {block.quotes.map((quote, i) => (
                     <div key={i} className="columns small-12">
-                        {quote.quote && <h3 className="om dash">{quote.quote}</h3>}
+                        {quote.quote && <h3 className="om dash">"{quote.quote}"</h3>}
                         {quote.author && <p className="om fade-up">{quote.author}</p>}
                     </div>
                 ))}
